600cced47eeaa2f144f84fd0bfa12b7597322411
François Parmentier <francois.parmentier@gmail.com>
2014-07-30 14:31:17 +0200

    Fix mistyping for castor.

--- downstream/overview-docs.js
+++ downstream/overview-docs.js
@@ -2,7 +2,7 @@
 
 var path = require('path')
   , basename = path.basename(__filename, '.js')
-  , debug = require('debug')('catsor:' + basename)
+  , debug = require('debug')('castor:' + basename)
   , util = require('util')
   , datamodel = require('datamodel')
   , render = require('../helpers/render.js')
