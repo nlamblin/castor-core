4b3706451780689a24f928242bc46f7bf22ee091
François Parmentier <francois.parmentier@gmail.com>
2014-09-23 17:32:01 +0200

    Use dotted notation instead of XPath (userfields)

--- loaders/set-userfields.js
+++ loaders/set-userfields.js
@@ -1,42 +1,29 @@
+/*jshint node:true, laxcomma:true*/
 'use strict';
 
 var path = require('path')
 , basename = path.basename(__filename, '.js')
 , debug = require('debug')('castor:loaders:' + basename)
 , path = require('path')
-, jsel = require('jsel')
+, objectPath = require('object-path')
 ;
 
 module.exports = function(config) {
   var fields = config.get('userfields');
 
   return function (input, submit) {
-    var values = {}, dom = jsel(input);
+    var values = {};
     if (typeof fields === 'object') {
       Object.keys(fields).forEach(function (key) {
           var xpr = fields[key];
           if (typeof xpr !== 'string' || xpr === '') {
             return;
           }
-          var val = dom.selectAll(xpr), vals = dom.select(xpr);
-          if (Array.isArray(val)) {
-            if (vals.length === 0) {
-              values[key] = val;
-            }
-            else if (vals.length === 1) {
-              values[key] = val;
-            }
-            else {
-              values[key] = vals;
-            }
-          }
-          else {
-            values[key] = val;
-          }
+          values[key] = objectPath.get(input, xpr);
         }
       );
       input['userfields'] = values;
     }
     submit(null, input);
-  }
-}
+  };
+};
