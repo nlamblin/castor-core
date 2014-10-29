d7c8f4391345e54d7a985683cbe9a6394a41d5c6
Nicolas Thouvenin <nthouvenin@gmail.com>
2014-10-29 11:18:55 +0100

    add missing file

---
+++ loaders/file.js
@@ -0,0 +1,23 @@
+'use strict';
+var path = require('path')
+  , basename = path.basename(__filename, '.js')
+  , path = require('path')
+  , fs = require('fs')
+  , prettysize = require('prettysize')
+  , mimetype = require('mimetype')
+  , shorthash = require('shorthash')
+  ;
+module.exports = function(options) {
+  options = options || {};
+  return function (input, submit) {
+    // Short ID
+    input.wid = shorthash.unique(input.fid);
+    // https://schema.org/fileSize
+    input.fileSize = prettysize(input.filesize);
+    // https://schema.org/fileFormat
+    input.fileFormat = mimetype.lookup(input.location) || undefined;
+    // https://schema.org/name
+    input.name = path.basename(input.location, path.extname(input.location)).replace(/[\_\-\.]+/g, ' ');
+    submit(null, input);
+  }
+}
