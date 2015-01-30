096a7ac5b38eb6f9675f1e8d4e4ed56c1a5505ce
François Parmentier <francois.parmentier@gmail.com>
2015-01-30 14:49:54 +0100

    :bug: Fix unique wid again.

--- loaders/append.js
+++ loaders/append.js
@@ -8,7 +8,7 @@ module.exports = function(options) {
   options = options || {};
   return function (input, submit) {
     // Short ID
-    input.wid = shorthash.unique(input.fid);
+    input.wid = shorthash.unique(input.fid + input.number);
     submit(null, input);
   }
 }
