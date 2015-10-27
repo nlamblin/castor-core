e8bb53b7273b2e43921f6b29715b1ab7c29445e2
Nicolas Thouvenin <nthouvenin@gmail.com>
2015-10-27 13:49:25 +0100

    remove useless package

--- app/loaders/prepend.js
+++ app/loaders/prepend.js
@@ -2,18 +2,10 @@
 var path = require('path')
   , basename = path.basename(__filename, '.js')
   , path = require('path')
-  , fs = require('fs')
-  , prettysize = require('prettysize')
-  , mimetype = require('mimetype')
   ;
 module.exports = function(options) {
   options = options || {};
   return function (input, submit) {
-    // https://schema.org/fileSize
-    input.fileSize = prettysize(input.filesize);
-    // https://schema.org/fileFormat
-    input.fileFormat = mimetype.lookup(input.location) || undefined;
-    // https://schema.org/name
     input.name = path.basename(input.location, path.extname(input.location)).replace(/[\_\-\.]+/g, ' ');
     // Pseudo search
     if (input.text) {
