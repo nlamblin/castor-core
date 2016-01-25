84840696dcb6d61b7448bd25be1605b4c4894a89
Nicolas Thouvenin <nthouvenin@gmail.com>
2016-01-25 23:43:18 +0100

    change default cols

--- app/models/mongo.js
+++ app/models/mongo.js
@@ -23,65 +23,76 @@ module.exports = function(model) {
         "_label": req.config.get('title'),
         "_text": req.config.get('description'),
         "_hash": null,
-        "_template": "<ul><li>_wid : {{_wid}}</li><li>_url : <a href=\"{{_url|e}}\">{{$_url}}</a></li></ul><ol> {% for doc in _documents %} <li><ul><li>_wid : {{_wid}}</li><li>_label : {{_label}}</li><li>_text : {{_text}}</li><li>_hash : {{_hash}}</li></ul></li> {% endfor %}</ol>",
+        "_template": null,
         "_root": true,
         "_columns" : {
           "_wid" : {
             //
             // Mandatory Column for the reduce system
             //
-            "label" : "Identifier",
-            "scheme": "http://purl.org/dc/elements/1.1/identifier",
+            "label" : "URI",
+            "scheme": "https://www.w3.org/ns/rdfa#uri",
             "comment" : "A mandatory column for \"dollar\" URLs",
             "title" : {
               "get" : "_label"
             }
           },
-          "_url" : {
+          "title" : {
+            "label" : "Title",
+            "scheme": "https://schema.org/title",
+            "type": "https://www.w3.org/TR/xmlschema-2/#string",
+            "get": "_label"
+          },
+          "description" : {
+            "label" : "Description",
+            "scheme": "https://schema.org/description",
+            "type": "https://www.w3.org/TR/xmlschema-2/#string",
+            "get": "_text"
+          },
+          "url" : {
             "label" : "URL",
-            "comment" : "Recommended Column to expose existing table",
             "scheme": "http://schema.org/url",
             "type": "http://www.w3.org/TR/xmlschema-2/#anyURI",
             "get": ["baseURL", "_wid"],
             "join": "/",
-            "title": {
-              "get" : "_wid"
-            }
+            // "title": {
+            // "get" : "_wid"
+            // }
           },
+          }
+        };
+        fill(index);
+    })
+    .prepend('mongoCollectionsIndexHandle', function(req, fill) {
+        var self = this;
+        if (self.mongoDatabaseHandle instanceof Error) {
+          return fill();
         }
-      };
-      fill(index);
-  })
-  .prepend('mongoCollectionsIndexHandle', function(req, fill) {
-      var self = this;
-      if (self.mongoDatabaseHandle instanceof Error) {
-        return fill();
-      }
-      self.mongoDatabaseHandle.collection(req.config.get('collectionsIndexName'), {strict:true}, function(err, coll) {
-          if (err) {
-            debug('Try to init index', req.config.get('collectionsIndexName'));
-            self.mongoDatabaseHandle.collection(req.config.get('collectionsIndexName'), function(err, newcoll) {
-                newcoll.insertOne(self.indexDescription).then(function() {
-                    self.mongoDatabaseHandle.createIndex(req.config.get('collectionsIndexName'),
-                      {_wid:1},
-                      {unique:true, background:false, w:1}
-                    ).then(function() {
-                        fill(newcoll);
-                    }).catch(fill);
-                }).catch(fill);
-            });
-          }
-          else {
-            fill(coll);
-          }
-      });
-  })
-  .complete('mongoDatabaseHandle', function(req, fill) {
-      if (this.mongoDatabaseHandle instanceof Error || this.mongoCursor !== undefined) {
-        return fill(this.mongoDatabaseHandle);
-      }
-      this.mongoDatabaseHandle.close().then(fill).catch(fill);
-  })
-  return model;
-}
+        self.mongoDatabaseHandle.collection(req.config.get('collectionsIndexName'), {strict:true}, function(err, coll) {
+            if (err) {
+              debug('Try to init index', req.config.get('collectionsIndexName'));
+              self.mongoDatabaseHandle.collection(req.config.get('collectionsIndexName'), function(err, newcoll) {
+                  newcoll.insertOne(self.indexDescription).then(function() {
+                      self.mongoDatabaseHandle.createIndex(req.config.get('collectionsIndexName'),
+                        {_wid:1},
+                        {unique:true, background:false, w:1}
+                      ).then(function() {
+                          fill(newcoll);
+                      }).catch(fill);
+                  }).catch(fill);
+              });
+            }
+            else {
+              fill(coll);
+            }
+        });
+    })
+    .complete('mongoDatabaseHandle', function(req, fill) {
+        if (this.mongoDatabaseHandle instanceof Error || this.mongoCursor !== undefined) {
+          return fill(this.mongoDatabaseHandle);
+        }
+        this.mongoDatabaseHandle.close().then(fill).catch(fill);
+    })
+    return model;
+  }
 
