d0779326b47f41354eb87fbdb624e1c2192dbe4f
Nicolas Thouvenin <nthouvenin@gmail.com>
2016-03-02 22:24:23 +0100

    insert hotfolder description automaticaly

--- app/models/mongo.js
+++ app/models/mongo.js
@@ -62,37 +62,80 @@ module.exports = function(model) {
           }
         };
         fill(index);
-    })
-    .prepend('mongoCollectionsIndexHandle', function(req, fill) {
+      })
+      .declare('hotfolderDescription', function(req, fill) {
+        var index;
+        if (req.config.has('dataPath')) {
+          index = {
+            // @todo Idéalement il faudrait inserer ce document avec castor-load
+            "fid": "hotfolder",       // pour être compatible castor-load
+            "number": 0,          // pour être compatible castor-load
+            "state": "inserted",  // pour être compatible castor-load
+            "_wid": "hotfolder",
+            "_label": "Hotfolder",
+            "_text": "The hot folder is continuously monitored, and when files are copied or dropped into it, they are automatically processed",
+            "_hash": null,
+            "_template": null,
+            "_root": false,
+            "_columns" : {
+              "uri" : {
+                //
+                // Mandatory Column for the reduce system
+                //
+                "label" : "URI",
+                "scheme": "https://www.w3.org/ns/rdfa#uri",
+                "comment" : "A mandatory column for \"dollar\" URLs",
+                "title" : {
+                  "get" : "_label"
+                }
+              },
+              "title" : {
+                "label" : "Title",
+                "scheme": "https://schema.org/title",
+                "type": "https://www.w3.org/TR/xmlschema-2/#string",
+                "get": "_label"
+              },
+              "description" : {
+                "label" : "Description",
+                "scheme": "https://schema.org/description",
+                "type": "https://www.w3.org/TR/xmlschema-2/#string",
+                "get": "_text"
+              }
+            }
+          };
+        }
+        fill(index);
+      })
+      .prepend('mongoCollectionsIndexHandle', function(req, fill) {
         var self = this;
         if (self.mongoDatabaseHandle instanceof Error) {
           return fill();
         }
         self.mongoDatabaseHandle.collection(req.config.get('collectionsIndexName'), {strict:true}, function(err, coll) {
-            if (err) {
-              debug('Try to init index', req.config.get('collectionsIndexName'));
-              self.mongoDatabaseHandle.collection(req.config.get('collectionsIndexName'), function(err, newcoll) {
-                  newcoll.insertOne(self.indexDescription).then(function() {
-                      self.mongoDatabaseHandle.createIndex(req.config.get('collectionsIndexName'),
-                        {_wid:1},
-                        {unique:true, background:false, w:1}
-                      ).then(function() {
-                          fill(newcoll);
-                      }).catch(fill);
-                  }).catch(fill);
-              });
-            }
-            else {
-              fill(coll);
-            }
+          if (err) {
+            debug('Try to init index', req.config.get('collectionsIndexName'));
+            self.mongoDatabaseHandle.collection(req.config.get('collectionsIndexName'), function(err, newcoll) {
+              newcoll.insertMany([self.indexDescription, self.hotfolderDescription]).then(function() {
+                self.mongoDatabaseHandle.createIndex(req.config.get('collectionsIndexName'),
+                  {_wid:1},
+                  {unique:true, background:false, w:1}
+                ).then(function() {
+                  fill(newcoll);
+                }).catch(fill);
+              }).catch(fill);
+            });
+          }
+          else {
+            fill(coll);
+          }
         });
-    })
-    .complete('mongoDatabaseHandle', function(req, fill) {
+      })
+      .complete('mongoDatabaseHandle', function(req, fill) {
         if (this.mongoDatabaseHandle instanceof Error || this.mongoCursor !== undefined) {
           return fill(this.mongoDatabaseHandle);
         }
         this.mongoDatabaseHandle.close().then(fill).catch(fill);
-    })
-    return model;
-  }
+      })
+      return model;
+    }
 
