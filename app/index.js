c5a4b9a013aa22db84e599dd7003d8f3409b0f64
François Parmentier <francois.parmentier@gmail.com>
2016-03-11 18:24:50 +0100

    :bug: Make JBJ able to use its modules/filters

--- app/index.js
+++ app/index.js
@@ -398,7 +398,7 @@ module.exports = function(config, online) {
   filters.from(extensionPath, __dirname)
   filters.over(config.get('filters'))
   filters.apply(function(hash, func) {
-    JBJ.use(func());
+    JBJ.use(func);
   });
   Object.keys(JBJ.filters).forEach(function(filterName) {
     env.addFilter(filterName, JBJ.filters[filterName]);
