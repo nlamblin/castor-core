59bc07d90e8970fa82dd77c7ac226fdb527657dc
Nicolas Thouvenin <nthouvenin@gmail.com>
2016-03-25 15:32:35 +0100

    fix default description for all tables that no registred in index

--- app/models/get-table.js
+++ app/models/get-table.js
@@ -8,6 +8,54 @@ var path = require('path')
 
 module.exports = function(model) {
   model
+    .declare('defaultDescription', function(req, fill) {
+    fill({
+      "fid": "__change__me__",       // pour être compatible castor-load
+      "number": 0,          // pour être compatible castor-load
+      "state": "inserted",  // pour être compatible castor-load
+      "_wid": "__change__me__",
+      "_label": 'Default Description',
+      "_text": "This is the default description for all tables that are not registered in the index.",
+      "_hash": null,
+      "_template": null,
+      "_root": true,
+      "_columns" : {
+        "_wid" : {
+          //
+          // Mandatory Column for the reduce system
+          //
+          "label" : "URI",
+          "scheme": "https://www.w3.org/ns/rdfa#uri",
+          "comment" : "A mandatory column for \"dollar\" URLs",
+          "title" : {
+            "get" : "_label"
+          }
+        },
+        "title" : {
+          "label" : "Title",
+          "scheme": "https://schema.org/title",
+          "type": "https://www.w3.org/TR/xmlschema-2/#string",
+          "get": "_label"
+        },
+        "description" : {
+          "label" : "Description",
+          "scheme": "https://schema.org/description",
+          "type": "https://www.w3.org/TR/xmlschema-2/#string",
+          "get": "_text"
+        },
+        "url" : {
+          "label" : "URL",
+          "scheme": "http://schema.org/url",
+          "type": "http://www.w3.org/TR/xmlschema-2/#anyURI",
+          "get": ["baseURL", "_wid"],
+          "join": "/"
+          // "title": {
+          // "get" : "_wid"
+          // }
+        }
+      }
+    });
+  })
   .append('table', function(req, fill) {
       var Errors = req.core.Errors;
       var self = this;
@@ -22,18 +70,17 @@ module.exports = function(model) {
                 fill(self.indexDescription);
             }).catch(fill);
           }
-          else if (doc === null && req.routeParams.resourceName !== 'index') {
-            fill(new Errors.TableNotFound('The table does not exist.'));
-          }
-          else if (doc._columns !== undefined) {
-            fill(doc);
-          }
-          else if (doc._columns === undefined && req.config.has('flyingFields')) {
-            doc._columns = req.config.get('flyingFields');
-            fill(doc);
+          else if (doc === null) {
+            self.defaultDescription.fid = req.routeParams.resourceName;
+            self.defaultDescription._wid = req.routeParams.resourceName;
+            self.defaultDescription._label = 'Table ' + req.routeParams.resourceName;
+            fill(self.defaultDescription);
           }
           else if (doc._columns === undefined) {
             doc._columns = []
+            fill(doc);
+          }
+          else {
             fill(doc);
           }
       }).catch(fill);
